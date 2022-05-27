ebb9c29914d5810967e92cded07d672554d65600
Rahul Singh <rahul.singh12221998@gmail.com>
2022-05-27 13:26:35 +0530

    fix styles

--- src/components/GitHubSponsorCard/index.tsx
+++ src/components/GitHubSponsorCard/index.tsx
@@ -3,12 +3,10 @@ import styles from "./styles.module.css";
 
 export default function GitHubSponsorCard(): JSX.Element {
   return (
-    <section id="sponsor">
+    <section id="sponsor" className={styles.sponsorSection}>
       <iframe
         src="https://github.com/sponsors/drph4nt0m/card"
         title="Sponsor drph4nt0m"
-        height="225"
-        width="600"
         className={styles.sponsorCard}
       ></iframe>
     </section>
